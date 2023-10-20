d2c776276c5f689c78475a0241e87d8c11eab371
Arsham <arsham.ramezani@gmail.com>
2023-10-20 08:37:28 -0700

    chore: build file removed

--- build/env.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function env(name) {
-    const _ = process.env[name];
-    if (!_)
-        throw new Error('No value for env variable : ' + name);
-    return _;
-}
-exports.default = env;
